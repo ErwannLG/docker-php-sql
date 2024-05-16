b717814dd78c366d0c7776ab80d9095a07c860b8
ErwannLG <erwannj.legall@gmail.com>
2024-05-16 12:27:22 +0200

    exemple de connexion et requête sql + fichier sql

---
+++ data/users.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : db
+-- Généré le : jeu. 16 mai 2024 à 09:34
+-- Version du serveur : 8.0.37
+-- Version de PHP : 8.2.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `projet`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`id`, `first_name`, `last_name`) VALUES
+(1, 'Bob', 'Machin'),
+(2, 'Patrick', 'Truc');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- src/index.php
+++ src/index.php
@@ -1,3 +1,36 @@
+<?php
+const DBHOST = 'db';
+const DBNAME = 'projet';
+const DBUSER = 'test';
+const DBPASS = 'test';
+
+$dsn = 'mysql:host=' . DBHOST . ';dbname=' . DBNAME . ';charset=utf8';
+
+try {
+    $db = new PDO($dsn, DBUSER, DBPASS);
+
+    echo "Connexion réussie 👍❤️♥</br>";
+  }
+  // On capture les exceptions et on affiche le message d'erreur
+  catch(PDOException $e){
+    echo "connection failed: " . $e->getMessage() . "</br>";
+}
+
+$sql = "SELECT * FROM users";
+// Préparation de la requête
+$query = $db->prepare($sql);
+// Exécution de la requête
+$query->execute();
+// Stockage du résultat dans un tableau associatif
+$users = $query->fetchAll(PDO::FETCH_ASSOC);
+
+$db = null;
+
+// echo "<pre>";
+// print_r($users);
+// echo "</pre>";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +39,13 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Hello World</h1>
+    <h1>Mon premier site qui fonctionne sur Docker! Ou pas?</h1>
+    <?php
+        foreach($users as $user) {
+    ?>
+        <div><?= $user['first_name'] . ' ' . $user['last_name'] ?> est gentil</div>
+    <?php
+        }
+    ?>
 </body>
 </html>
